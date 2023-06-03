94807aa6959f94c8d67a8c4998943dda620b082f
AdemfyWan <128696463+AdemfyWan@users.noreply.github.com>
2023-06-03 12:16:57 +0200

    Add files via upload

--- AssignmentMain/battery.py
+++ AssignmentMain/battery.py
@@ -5,14 +5,22 @@ from parapy.geom import *
 class Battery(Box):
     w_c_root = Input(0.4243)
     payload_weight = Input(0.5)
+    Endurance = Input(60)     #[min]
+    V_cruise = Input(15)     #[m/s]
+    Cd = Input(0.0106)
+    ref_area = Input(0.95822)  #[m^2]
+    Energy_density = Input(500) #[Wh/L]
+    Specific_energy = Input(200)  #[Wh/kg]
+    air_density = Input(1.1911)  #[kg/m^3]
+
 
     @Input
     def height(self):  # z  # TO BE UPDATED
-        return 0.026 * self.w_c_root/0.4342
+        return self.battery_volume/self.length/self.width
 
     @Input
     def length(self):  # y  # TO BE UPDATED
-        return 0.139 * self.w_c_root / 0.4342
+        return 0.12 * self.w_c_root / 0.4342
 
     @Input
     def width(self):  # x   # TO BE UPDATED
@@ -20,7 +28,11 @@ class Battery(Box):
 
     @Attribute
     def battery_weight(self):  # TO BE UPDATED
-        return self.payload_weight*2
+        return 0.5*self.air_density * self.V_cruise**3 * self.ref_area * self.Cd * self.Endurance/60 / self.Specific_energy
+
+    @Attribute
+    def battery_volume(self):   # [m^3]
+        return 0.5*self.air_density * self.V_cruise**3 * self.ref_area * self.Cd * self.Endurance/60 / self.Energy_density / 1000
 
 
 if __name__ == '__main__':
